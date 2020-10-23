70c5a19de1cf345256cae6004a812de12dc871f0
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-10-23 13:27:56 +0300

    ınput compenenent

---
+++ newsPage/src/login.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+import { SafeAreaView, View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+
+import {Input, Button} from './compenents';
+
+const login = () => {
+    
+    return(
+        <SafeAreaView style={{flex: 1, backgroundColor: '#b2dfdb'}}>
+            <View style={{flex: 1}}>
+                <Image source={require('./assets/16036.png')}
+                style={styles.logoStyle}/>
+                <Input holder="Please Enter E-Mail Adress"/>
+                <Input holder="Please Enter Password"/>
+                
+            </View>
+        </SafeAreaView>
+
+    )
+}
+
+export default login;
+
+const styles = StyleSheet.create({
+    logoStyle: {
+        resizeMode: "contain",
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height * 0.40
+    },
+})
