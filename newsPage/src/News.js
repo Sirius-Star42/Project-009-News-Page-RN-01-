dec8640d54c6e491078fdcb2663cde8cca18e3d8
Sirius-Star42 <67394008+Sirius-Star42@users.noreply.github.com>
2020-10-25 16:44:54 +0300

    compenets added and placed images

--- newsPage/src/News.js
+++ newsPage/src/News.js
@@ -1,13 +1,63 @@
 import React from 'react';
-import { SafeAreaView, View, Text,StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, ScrollView, FlatList } from "react-native";
+import { NewsCard } from "./compenents";
 
+const myArray = [1, 2, 3, 4];
+const mydata = [{
+    id:0,
+    title:'McDonnell Douglas F-15E Strike Eagle',
+    desc:'The McDonnell Douglas (now Boeing) F-15E Strike Eagle is an American all-weather multirole strike fighter[6] derived from the McDonnell Douglas F-15 Eagle. The F-15E was designed in the 1980s for long-range, high-speed interdiction without relying on escort or electronic-warfare aircraft. ',
+    img_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/F-15E_Strike_Eagle.jpg/300px-F-15E_Strike_Eagle.jpg',
+},
+{
+    id:1,
+    title:'Lockheed Martin F-22 Raptor',
+    desc:'The Lockheed Martin F-22 Raptor is a single-seat, twin-engine, all-weather stealth tactical fighter aircraft developed for the United States Air Force (USAF). The result of the USAF s Advanced Tactical Fighter (ATF) program, the aircraft was designed primarily as an air superiority fighter, but also has ground attack, electronic warfare, and signal intelligence capabilities',
+    img_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/F-22_Raptor_edit1_%28cropped%29.jpg/300px-F-22_Raptor_edit1_%28cropped%29.jpg',
+},
+{
+    id:2,
+    title:'Lockheed Martin F-35 Lightning II',
+    desc:'The Lockheed Martin F-35 Lightning II is an American family of single-seat, single-engine, all-weather stealth multirole combat aircraft that is intended to perform both air superiority and strike missions. It is also able to provide electronic warfare and intelligence, surveillance, and reconnaissance capabilities. Lockheed Martin is the prime F-35 contractor, with principal partners Northrop Grumman and BAE Systems.',
+    img_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/F-35A_flight_%28cropped%29.jpg/300px-F-35A_flight_%28cropped%29.jpg',
+},
+{
+    id:3,
+    title:'General Dynamics F-16 Fighting Falcon',
+    desc:'The General Dynamics F-16 Fighting Falcon is a single-engine supersonic multirole fighter aircraft originally developed by General Dynamics for the United States Air Force (USAF). Designed as an air superiority day fighter, it evolved into a successful all-weather multirole aircraft. ',
+    img_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/F-16_June_2008.jpg/300px-F-16_June_2008.jpg',
+},
+{
+    id:4,
+    title:'Fairchild Republic A-10 Thunderbolt II',
+    desc:'The Fairchild Republic A-10 Thunderbolt II is a single-seat, twin turbofan engine, straight wing jet aircraft developed by Fairchild-Republic for the United States Air Force (USAF). It is commonly referred to by the nicknames "Warthog" or "Hog", although the A-10 s official name comes from the Republic P-47 Thunderbolt, a World War II fighter-bomber effective at attacking ground targets',
+    img_url:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/A-10_-_32156159151.jpg/300px-A-10_-_32156159151.jpg',
+},
+];
+
+
+//const renderNews = ({item}) => { 
+//    return (<Text>{item.title}</Text>)};       
+     
+const renderNews = ({item}) => { 
+    return(
+    <NewsCard news={item}/>
+    )
+}; 
 const News = () => {
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>News</Text>
-            </View>
-        </SafeAreaView>
+        <SafeAreaView style={{flex:1}}>
+             
+            
+            <FlatList
+                keyExtractor={(item, index) => {item.id.toString()}}
+                data={mydata}
+                renderItem={renderNews}
+            />
+            
+        </SafeAreaView>  
+    
 
     )
 }
---
+++ newsPage/src/compenents/NewsCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Image, Text, StyleSheet, Dimensions } from "react-native";
+
+const NewsCard = ({news}) => {
+    return (
+        <View style={{flex: 1}}> 
+            <Image source={{uri: news.img_url}}
+            style={styles.image}
+            
+            />
+            <Text style={styles.title}>{news.title}</Text>
+            <Text style={styles.desc}>{news.desc}</Text>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    title:{
+        fontWeight: "bold",
+        textAlign: "center",
+        margin: 2,
+        fontSize: 22,
+    },
+    desc: {
+        textAlign: "justify",
+        margin: 8,
+        
+
+
+    },
+    image: {
+        height:Dimensions.get('window').height * 0.3,
+        resizeMode: 'cover',
+        margin: 5,
+    }
+
+})
+
+
+export {NewsCard} ;
